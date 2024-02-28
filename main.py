f8e0ff283ee607136c401866b565de7035244680
Andrei <yeloww51@gmail.com>
2024-02-28 23:30:42 +0300

    feat: add SQLSolution

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from database import CConnection, session_scope
-from sqlalchemy import text
+from solutions.sql_solution import SQLSolution
 
 
-connection = CConnection()
+def main():
+    sql_solution = SQLSolution()
+    sql_res = sql_solution.get_data()
+    print(sql_res)
 
-with session_scope(connection) as session:
-    # Тест соединения с БД
-    print(session.execute(text("SELECT 1")).fetchone())
 
+if __name__ == "__main__":
+    main()
